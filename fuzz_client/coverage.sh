72e2171227907a676332f8263b01bbff95196090
YeonHui829 <dusgml8829@gmail.com>
2024-01-25 11:21:37 +0900

    [UPDATE] coverage.sh

--- fuzz_client/coverage.sh
+++ fuzz_client/coverage.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-function_name="$1"
-result_file="$2"
-gcov -b -c -i -f test_client.gcda | awk -v fn="$function_name" '$0 ~ "^Function \047"fn"\047", /^Calls executed/' > "$result_file"
+target_file="$1"
+function_name="$2"
+result_file="$3"
+
+gcov -b -c -i -f "$target_file" | awk -v fn="$function_name" '$0 ~ "^Function \047"fn"\047", /^Calls executed/' > "$result_file"
